1c072c92e0838db940509a679d60e9d2f5eb3c8c
Jefferson Ventura <jeffersonsilvamuz@gmail.com>
2023-12-19 23:34:25 -0300

    refactor: Refactor logout logic to ensure the request is of type POST. Update the logout URL and adjust the HTML code in the menu to replace the logout button with a form that submits a POST request. Add CSRF token to the form for security.

--- API/urls.py
+++ API/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.contrib.auth import views as auth_views
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -42,6 +43,11 @@ urlpatterns = [
     # Admin
     path('admin/', admin.site.urls),
     
+    path('auth/', include("rest_framework.urls")),
+
     # Apps
     path('', include("Task.urls")),
+
+    # Logout
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
